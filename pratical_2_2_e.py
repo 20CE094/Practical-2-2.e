b7c3a2cb84d98da4462e638a2546c87efca3355f
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-15 00:49:24 +0530

    Add files via upload

---
+++ pratical_2_2_e.py
@@ -0,0 +1,8 @@
+#Name-Nehal Patel
+#ID- 20CE094
+#Github link: https://github.com/20CE094/Practical-2-2.e.git
+#Aim: Write a Python program to find the length of a tuple
+t=(1,2,3,4,8,9)
+print(t)
+#using len function to find length of tuple
+print("Length of the tuple is ",len(t))
